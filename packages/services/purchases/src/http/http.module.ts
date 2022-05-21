bb900cf1e4952bde24f6924e5276069fcaa2410c
Leonardo Dias Lissone Santomero <leonardo.lissonez@gmail.com>
2022-05-21 19:24:39 -0300

    feat(purchases): list all products and create product

--- packages/services/purchases/src/http/http.module.ts
+++ packages/services/purchases/src/http/http.module.ts
@@ -1,11 +1,13 @@
-import { ApolloDriver } from '@nestjs/apollo'
-import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
-import { GraphQLModule } from '@nestjs/graphql'
-import path from 'node:path'
+import { ApolloDriver } from "@nestjs/apollo";
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { GraphQLModule } from "@nestjs/graphql";
+import path from "node:path";
 
-import { DatabaseModule } from '../database/database.module'
-import { TestResolver } from './test.resolver'
+import { ProductsService } from "@services/products.service";
+
+import { DatabaseModule } from "../database/database.module";
+import { ProductsResolver } from "./graphql/resolvers/products.resolver";
 
 @Module({
   imports: [
@@ -13,9 +15,9 @@ import { TestResolver } from './test.resolver'
     DatabaseModule,
     GraphQLModule.forRoot({
       driver: ApolloDriver,
-      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql')
-    })
+      autoSchemaFile: path.resolve(process.cwd(), "src/schema.gql"),
+    }),
   ],
-  providers: [TestResolver]
+  providers: [ProductsResolver, ProductsService],
 })
 export class HttpModule {}
